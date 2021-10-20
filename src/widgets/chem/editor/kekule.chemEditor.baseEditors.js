9396ab332e95050c2a8c68e394277ab2632160d4
Tahar Meftah <tahar.meftah@macmillan.com>
2021-10-20 11:47:05 -0700

    code review

--- src/widgets/chem/editor/kekule.chemEditor.baseEditors.js
+++ src/widgets/chem/editor/kekule.chemEditor.baseEditors.js
@@ -4080,7 +4080,6 @@ Kekule.Editor.BaseEditor = Class.create(Kekule.ChemWidget.ChemObjDisplayer,
 				if (o)
 					this.operationDone(o);
 			}
-			this._removeOrphans('undo');
 		}
 		return o;
 	},
@@ -4104,18 +4103,8 @@ Kekule.Editor.BaseEditor = Class.create(Kekule.ChemWidget.ChemObjDisplayer,
 				if (o)
 					this.operationDone(o)
 			}
-			this._removeOrphans('redo');
 		}
 		return o;
-	},
-	_removeOrphans: function(action) {
-		const chemSpace = this.getChemSpace();
-		if (chemSpace) {
-			const children = chemSpace.getChildren().filter(x => x.CLASS_NAME === 'Kekule.Glyph.PathGlyphArcConnectorControlNode');
-			// for (const child of children) {
-			// 	      child.getParent().removeChild(child);
-			// }
-		}
 	},
 	/**
 	 * Undo all operations.
